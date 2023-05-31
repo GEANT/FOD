7743dd5e1f71ccad1cc13d2a7d1384cdcbadcc98
David Schmitz <schmitz@lrz.de>
2023-05-31 07:49:04 +0000

    feature/ruletable_dynamic_columns: arrangement of the buttons; fix sorting of entries in table default by name again; dont reset paging on reload

--- templates/pollerdash.js
+++ templates/pollerdash.js
@@ -65,7 +65,8 @@ function reloadDataTable(cause) {
        //oTable.fnReloadAjax(refreshUrl);
        //console.log("oTable="+oTable);
        //console.log("ajax2="+$('#routes_table').DataTable().ajax);
-       $('#routes_table').DataTable().ajax.reload();
+       //$('#routes_table').DataTable().ajax.reload();
+       $('#routes_table').DataTable().ajax.reload(null, false);
      } else {
        console.log("DataTable.ajax is undefined");
        //oTable.fnReloadAjax(refreshUrl);
