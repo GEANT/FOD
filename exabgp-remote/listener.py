af200b9c262f8dbdd2fb277748ed7c73f564fff5
Anna Wilson <anna.wilson@heanet.ie>
2023-09-13 14:25:18 +0000

    config template for the exabgp listener

--- exabgp-remote/listener.py
+++ exabgp-remote/listener.py
@@ -2,11 +2,12 @@ import os
 import requests
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def trigger_config_update():
     FOD_API_URL = os.getenv('FOD_API_URL')
     FOD_API_TOKEN = os.getenv('FOD_API_TOKEN')
 
@@ -15,5 +16,15 @@ def hello_world():
 
     r = requests.get(url, headers=headers)
 
-    return r.text
+    output = render_template('exabgp_conf.j2',
+                             flow_conf=r.text,
+                             local_asn=os.getenv('LOCAL_ASN'),
+                             local_ip=os.getenv('LOCAL_IP'),
+                             local_nodeid=os.getenv('LOCAL_NODEID'),
+                             remote_asn=os.getenv('REMOTE_ASN'),
+                             remote_ip=os.getenv('REMOTE_IP'),
+                             remote_nodeid=os.getenv('REMOTE_NODEID'),
+                             )
+
+    return output
 
