14c41a418d9d82377b1e3463fa5baa072614efa5
David Schmitz <schmitz@lrz.de>
2024-06-19 09:21:39 +0000

    testing: update inst/testing/nemo1/

---
+++ inst/testing/nemo1/init-vsmd1-container.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+
+if [ ! -f /nemo-all/secrets/vmsd1.site.crt.pem ]; then
+  /nemo-all/install_vsmd_certs
+fi
+
+##
+
+docker build -f Dockerfile-vsmd1 -t vsmd1 . 
+
+docker stop vsmd1
+docker rm vsmd1
+
+docker run -d \
+	--privileged --network host \
+	--name vsmd1 \
+	--mount type=bind,source=/nemo-all/etc/,target=/nemo-all/etc/ \
+	--mount type=bind,source=/nemo-all/secrets/,target=/nemo-all/secrets/ \
+	vsmd1
+
+docker exec -ti vsmd1 ./install_and_run_vsmd
