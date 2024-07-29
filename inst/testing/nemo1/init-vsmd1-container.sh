68fa30503dc2a0cd6d99d566dce534b37ade08ec
David Schmitz <schmitz@lrz.de>
2024-07-29 08:39:10 +0000

    testing: update inst/testing/nemo1/

--- inst/testing/nemo1/init-vsmd1-container.sh
+++ inst/testing/nemo1/init-vsmd1-container.sh
@@ -13,17 +13,24 @@ fi
 
 ##
 
-docker build -f Dockerfile-vsmd1 -t vsmd1 . 
+container_name="$1"
+shift 1
 
-docker stop vsmd1
-docker rm vsmd1
+[ -n "$container_name" ] || container_name="vsmd1"
+
+##
+
+docker build -f Dockerfile-vsmd1 -t "$container_name" . 
+
+docker stop "$container_name"
+docker rm "$container_name"
 
 docker run -d \
 	--privileged --network host \
-	--name vsmd1 \
+	--name "$container_name" \
 	--mount type=bind,source=/nemo-all/etc/,target=/nemo-all/etc/ \
 	--mount type=bind,source=/nemo-all/secrets/,target=/nemo-all/secrets/ \
-	vsmd1
+	"$container_name"
 
-docker exec -ti vsmd1 ./install_and_run_vsmd
+docker exec -ti "$container_name" ./install_and_run_vsmd
 
