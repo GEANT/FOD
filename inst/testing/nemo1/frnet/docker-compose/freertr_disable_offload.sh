69f16a7fde88dd38a22083ab23b614716607882d
David Schmitz <schmitz@lrz.de>
2025-04-17 14:34:15 +0000

    testing: update inst/testing/nemo1/: update

--- inst/testing/nemo1/frnet/docker-compose/freertr_disable_offload.sh
+++ inst/testing/nemo1/frnet/docker-compose/freertr_disable_offload.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 
-for container_interface in 0 1 2 3 4 5 6; do
-  #IFINDEX=$(docker exec freertr cat /sys/class/net/eth0/iflink)
-  IFINDEX=$(docker exec freertr cat "/sys/class/net/eth$container_interface/iflink")
-  IFNAME=$(ip a | grep ^${IFINDEX} | awk -F\: '{print $2}' | awk -F\@ '{print $1}')
-  echo "IFINDEX=$IFINDEX => IFNAME=$IFNAME" 1>&2
+for container_interface in 0 1 2 3 4 5; do
+  IFINDEX="$(docker exec freertr cat "/sys/class/net/eth$container_interface/iflink")"
+  [ -n "$IFINDEX" ] || continue
+
+  IFNAME="$(ip a | grep ^${IFINDEX} | awk -F\: '{print $2}' | awk -F\@ '{print $1}')"
+  IFNAME="${IFNAME# }"
+  [ -n "$IFNAME" ] || continue
+
+  echo "# container_interface eth$container_interface => IFINDEX=$IFINDEX => IFNAME of pair endpoint interface in current net ns=$IFNAME" 1>&2
 
   { 
     echo rx 
@@ -21,11 +25,12 @@ for container_interface in 0 1 2 3 4 5 6; do
     echo ntuple 
     echo rxhash 
 
-    ethtool -k $IFNAME | awk '{ sub(/^\s+/, ""); } $2=="on" { sub(/:$/, "", $1); print $1; }' 
+    ethtool -k "$IFNAME" | awk '{ sub(/^\s+/, ""); } $2=="on" { sub(/:$/, "", $1); print $1; }' 
 
-  } | while read key; do (ethtool -K $IFNAME "$key" off); done
+  } | while read key; do (ethtool -K "$IFNAME" "$key" off); done
 
-  ethtool -k $IFNAME | grep " on"
+  echo "# resulting ethtool settings for interface $IFNAME (pair end point of eth$container_interface in container freertr):"
+  ethtool -k "$IFNAME" | grep " on"
   echo
 
 done
