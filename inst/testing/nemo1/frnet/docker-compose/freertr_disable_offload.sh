7204b0c6c13a56127eba32a1279870092cedd490
David Schmitz <schmitz@lrz.de>
2025-04-17 12:19:47 +0000

    testing: update inst/testing/nemo1/: update

--- inst/testing/nemo1/frnet/docker-compose/freertr_disable_offload.sh
+++ inst/testing/nemo1/frnet/docker-compose/freertr_disable_offload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for container_interface in 0 1 2 3; do
+for container_interface in 0 1 2 3 4 5 6; do
   #IFINDEX=$(docker exec freertr cat /sys/class/net/eth0/iflink)
   IFINDEX=$(docker exec freertr cat "/sys/class/net/eth$container_interface/iflink")
   IFNAME=$(ip a | grep ^${IFINDEX} | awk -F\: '{print $2}' | awk -F\@ '{print $1}')
