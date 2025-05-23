e7f6b63c75b86d521369f566fa44122f7ecbc05a
David Schmitz <schmitz@lrz.de>
2025-05-23 15:13:39 +0000

    testing: update inst/testing/nemo1/: update

--- inst/testing/nemo1/nemo-initial-detectors1.sql
+++ inst/testing/nemo1/nemo-initial-detectors1.sql
@@ -5,16 +5,6 @@
 -- Dumped from database version 12.22 (Debian 12.22-1.pgdg120+1)
 -- Dumped by pg_dump version 12.22 (Debian 12.22-1.pgdg120+1)
 
-
---
--- Data for Name: trigger; Type: TABLE DATA; Schema: public; Owner: nemo
---
-
-COPY public.trigger (id, name, description, trigger_type, autostart, email, description_template, subject_template, body_template, short_open_template, long_open_template, short_upgrade_template, long_upgrade_template, short_close_template, long_close_template, default_filter, do_not_send_mails, mail_analysis_severity) FROM stdin;
-1	evw		time_window	t	\N											f	100
-\.
-
-
 --
 -- Data for Name: detector; Type: TABLE DATA; Schema: public; Owner: nemo
 --
@@ -24,6 +14,7 @@ COPY public.detector (id, name, detector_type, num_threads, autostart, visibilit
 \.
 
 
+--
 -- Data for Name: detector_param; Type: TABLE DATA; Schema: public; Owner: nemo
 --
 
@@ -52,12 +43,58 @@ COPY public.tagged_item (id, object_id, content_type_id, tag_id, automatic) FROM
 
 
 --
+-- Data for Name: trigger; Type: TABLE DATA; Schema: public; Owner: nemo
+--
+
+COPY public.trigger (id, name, description, trigger_type, autostart, email, description_template, subject_template, body_template, short_open_template, long_open_template, short_upgrade_template, long_upgrade_template, short_close_template, long_close_template, default_filter, do_not_send_mails, mail_analysis_severity) FROM stdin;
+1	evw		time_window	t	\N											f	100
+2	evw2		time_window	t	\N											f	100
+\.
+
+
+--
 -- Data for Name: timewindowtrigger; Type: TABLE DATA; Schema: public; Owner: nemo
 --
 
 COPY public.timewindowtrigger (triggerconfig_ptr_id, per_object, per_event_type, per_detector, open_conditions, upgrade_conditions1, upgrade_conditions2, close_conditions) FROM stdin;
 1	f	f	t	{"severity":"0","op":"all","conds":[{"type":"timewindow","comp":"gt","count":"1","minutes":"5","filter":[{"type":"detector_id","comp":"eq","val":"1"}],"filter_op":"all"}]}	{"severity":"0","op":"all","conds":[]}	{"severity":"0","op":"all","conds":[]}	{"op":"all","conds":[]}
+2	f	t	f	{"severity":"2","op":"all","conds":[{"type":"timewindow","comp":"eq","count":"1","minutes":"5","filter":[{"type":"detector_id","comp":"eq","val":"1"},{"type":"severity","comp":"eq","val":"2"}],"filter_op":"all"}]}	{"severity":"0","op":"all","conds":[]}	{"severity":"0","op":"all","conds":[]}	{"op":"all","conds":[]}
 \.
+
+
+--
+-- Name: detector_id_seq; Type: SEQUENCE SET; Schema: public; Owner: nemo
+--
+
+SELECT pg_catalog.setval('public.detector_id_seq', 1, false);
+
+
+--
+-- Name: detector_param_id_seq; Type: SEQUENCE SET; Schema: public; Owner: nemo
+--
+
+SELECT pg_catalog.setval('public.detector_param_id_seq', 1, false);
+
+
+--
+-- Name: tag_id_seq; Type: SEQUENCE SET; Schema: public; Owner: nemo
+--
+
+SELECT pg_catalog.setval('public.tag_id_seq', 1, false);
+
+
+--
+-- Name: tagged_item_id_seq; Type: SEQUENCE SET; Schema: public; Owner: nemo
+--
+
+SELECT pg_catalog.setval('public.tagged_item_id_seq', 1, false);
+
+
+--
+-- Name: trigger_id_seq; Type: SEQUENCE SET; Schema: public; Owner: nemo
+--
+
+SELECT pg_catalog.setval('public.trigger_id_seq', 2, true);
 
 
 --
