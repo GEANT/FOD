f4755be09891d6737f87033f5976a885636b2ad3
David Schmitz <schmitz@lrz.de>
2025-09-11 14:19:44 +0000

    testing: update inst/testing/nemo1/: update

--- inst/testing/nemo1/mynemo-mitigation-fix-detection-containers.sh
+++ inst/testing/nemo1/mynemo-mitigation-fix-detection-containers.sh
@@ -11,26 +11,31 @@
 
 ##############################################################################
 
+DIDCLFS="_"
+[ ! -f ./nemo1.env ] || . ./nemo1.env
+
+#
+
 set -x
 
-docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_authframeinformation INFRA "Infrastructure Protection" 0.0.0.0/0,::/0 alwaystrue
+docker exec -ti "nemo${DIDCLFS}mitigated${DIDCLFS}1" /services/inst/nemo-erkennung/bin/nemo-dbadmin add_authframeinformation INFRA "Infrastructure Protection" 0.0.0.0/0,::/0 alwaystrue
 echo
 
 ##
 
 sed -i -e 's#^.*deactivate_replisync.*$#deactivate_replisync: 1#' /nemo-all/etc/nemo-analyse/fishtank/nemo.conf
-docker restart nemo_fishtank_1
+docker restart "nemo${DIDCLFS}fishtank${DIDCLFS}1"
 echo
 
 cp -f /nemo-all/create_vsmd1.py /nemo-all/etc/nemo-erkennung/mitigated/ # TODO: currently misuse of shared fs
-docker exec -ti nemo_mitigated_1 python3 /services/etc/nemo/create_vsmd1.py
+docker exec -ti "nemo${DIDCLFS}mitigated${DIDCLFS}1" python3 /services/etc/nemo/create_vsmd1.py
 
 ##
 
-#docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "attacker network" 10.1.10.0/24 
-#docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "victim network" 10.2.10.0/24 
-#docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "customer2 network" 10.3.10.0/24 
+#docker exec -ti nemo${DIDCLFS}mitigated${DIDCLFS}1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "attacker network" 10.1.10.0/24 
+#docker exec -ti nemo${DIDCLFS}mitigated${DIDCLFS}1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "victim network" 10.2.10.0/24 
+#docker exec -ti nemo${DIDCLFS}mitigated${DIDCLFS}1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "customer2 network" 10.3.10.0/24 
 #
-#docker exec -ti nemo_nemodb_1 psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES (1, 1), (1, 2), (1, 3)"
+#docker exec -ti nemo${DIDCLFS}nemodb${DIDCLFS}1 psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES (1, 1), (1, 2), (1, 3)"
 
 
--- inst/testing/nemo1/mynemo-mitigation-init-db-final.sh
+++ inst/testing/nemo1/mynemo-mitigation-init-db-final.sh
@@ -10,42 +10,44 @@ set -x
 attacker_network1="10.1.10.0/24"
 victim_network1="10.2.10.0/24"
 
+DIDCLFS="_"
+
 [ ! -f /nemo-all/nemo1.env ] || . /nemo-all/nemo1.env
 
 #
 
-#docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "attacker network" 10.1.10.0/24 
-#docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "victim network" 10.2.10.0/24 
-docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "attacker network" "$attacker_network1" 
-docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "victim network" "$victim_network1"
-#docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "victim host" 10.2.10.12/32
-#docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "attacker host" 10.1.10.11/32
+#docker exec -ti "nemo${DIDCLFS}mitigated${DIDCLFS}1" /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "attacker network" 10.1.10.0/24 
+#docker exec -ti "nemo${DIDCLFS}mitigated${DIDCLFS}1" /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "victim network" 10.2.10.0/24 
+docker exec -ti "nemo${DIDCLFS}mitigated${DIDCLFS}1" /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "attacker network" "$attacker_network1" 
+docker exec -ti "nemo${DIDCLFS}mitigated${DIDCLFS}1" /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "victim network" "$victim_network1"
+#docker exec -ti "nemo${DIDCLFS}mitigated${DIDCLFS}1" /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "victim host" 10.2.10.12/32
+#docker exec -ti "nemo${DIDCLFS}mitigated${DIDCLFS}1" /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "attacker host" 10.1.10.11/32
 
 idx=2
 #for add_net in $(docker exec -i freertr ip a | grep -o "veth[3-9]@"); do
 for add_net in $(grep -- "ipv4 address 10.[3-9].10.3" frnet/docker-compose/freertr.cfg); do
   idx=$(( $idx + 1 ))
   echo "$0: idx=$idx" 1>&2
-  docker exec -ti nemo_mitigated_1 /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "customer$idx network" "10.$idx.10.0/24"
+  docker exec -ti "nemo${DIDCLFS}mitigated${DIDCLFS}1" /services/inst/nemo-erkennung/bin/nemo-dbadmin add_net --comments "customer$idx network" "10.$idx.10.0/24"
 done
 
 #
 
-#docker exec -ti nemo_nemodb_1 psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES (1, 1), (1, 2), (2, 2), (3, 1)"
+#docker exec -ti "nemo${DIDCLFS}nemodb${DIDCLFS}1" psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES (1, 1), (1, 2), (2, 2), (3, 1)"
 set -x
 num=1
-echo "select id, name from router;" | docker exec -i nemo_nemodb_1 psql -U nemo -t | while read id sep1 name; do
+echo "select id, name from router;" | docker exec -i "nemo${DIDCLFS}nemodb${DIDCLFS}1" psql -U nemo -t | while read id sep1 name; do
   echo "loop id=$id <-> name=$name" 1>&2
  
   case "$name" in
 	  freertr1*) 
-            docker exec nemo_nemodb_1 psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES ($id, 1), ($id, 2)"
+            docker exec "nemo${DIDCLFS}nemodb${DIDCLFS}1" psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES ($id, 1), ($id, 2)"
           ;;
           host1*) 
-            docker exec nemo_nemodb_1 psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES ($id, 1)"
+            docker exec "nemo${DIDCLFS}nemodb${DIDCLFS}1" psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES ($id, 1)"
 	  ;;
 	  host2*) 
-            docker exec nemo_nemodb_1 psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES ($id, 2)"
+            docker exec "nemo${DIDCLFS}nemodb${DIDCLFS}1" psql -U nemo -c "INSERT INTO router_nets (router_id, net_id) VALUES ($id, 2)"
 	  ;;
   esac
 
@@ -55,10 +57,10 @@ done
 #
 
 
-cat /nemo-all/nemo-initial-detectors1.sql | docker exec -i nemo_nemodb_1 psql -U nemo nemo
+cat /nemo-all/nemo-initial-detectors1.sql | docker exec -i "nemo${DIDCLFS}nemodb${DIDCLFS}1" psql -U nemo nemo
 
 #
 
-docker restart nemo_eventd_1
+docker restart "nemo${DIDCLFS}eventd${DIDCLFS}1"
 
 
